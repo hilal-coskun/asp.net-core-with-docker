7f645c1bcb6f85c32525e4da4bc6c5109f06c004
hilal.coskun <hilal.coskun20@gmail.com>
2023-04-25 11:21:59 +0300

    create new project

---
+++ DockerVolumeApp/DockerVolumeWithAspNetCoreMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerVolumeWithAspNetCoreMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
